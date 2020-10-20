fd858a7b6ff7fb90ce52dc0aee0e5d713a57d176
Anis Mansour <anis@Aniss-MacBook-Pro.local>
2020-10-20 16:11:34 -0700

    side bar and body CSS

---
+++ src/SideBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './SideBar.css';
+
+function SideBar() {
+  return (
+    <div className="sidebar">
+      <img
+        className="sidebar_logo"
+        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+        alt="logo"
+      />
+    </div>
+  );
+}
+
+export default SideBar;
---
+++ src/reducer.js
@@ -0,0 +1,30 @@
+export const initialState = {
+  user: null,
+  playlist: [],
+  playing: false,
+  item: null,
+  //token: null,
+  //remove after token with token
+  token:
+    'BQBc7aRIrNo0cWJCVGuur0KByvg_jzR04ZdEaMHqFcRXcLHblz7P_N8TybBTZYKavcyL2kPeDr9fK7qjfW6oEAv0zmcHcdyn5M692n18IEsvwhGWnwwrpZr_pEIsjOO24M84RNCEXtT21r04ybqgVMrhZ1Qss3Rc',
+};
+
+const reducer = (state, action) => {
+  console.log(action);
+
+  switch (action.type) {
+    case 'SET_USER':
+      return {
+        ...state,
+        user: action.user,
+      };
+    case 'SET_TOKEN':
+      return {
+        ...state,
+        token: action.token,
+      };
+    default:
+      return state;
+  }
+};
+export default reducer;
