7f2196e32db6dd4f00f19f623a6f4c31aa519ca2
Anis Mansour <anis@Aniss-MacBook-Pro.local>
2020-10-19 17:00:12 -0700

    login page

---
+++ src/Login.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './Login.css';
+
+export class Login extends Component {
+  render() {
+    return (
+      <div className="login">
+        <img
+          src="https://www.theprp.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+          alt="logo"
+        />
+        <a href="ADD LINK TO SPOTIFY LOGIN">Login with Spotify</a>
+      </div>
+    );
+  }
+}
+
+export default Login;
