57b90219477d2025a425dd8461fc367197a93e64
Anis Mansour <anis@Aniss-MacBook-Pro.local>
2020-10-23 15:04:24 -0700

    added playlist body

--- src/App.js
+++ src/App.js
@@ -38,6 +38,12 @@ function App() {
           playlists: playlists,
         });
       });
+      spotify.getPlaylist('37i9dQZEVXcIJazRV9ISoM').then((Response) =>
+        dispatch({
+          type: 'SET_DISCOVER_WEEKLY',
+          discover_weekly: Response,
+        })
+      );
     }
   }, []);
   //console.log('the user is ', user);
