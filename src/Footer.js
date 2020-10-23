83fb087b0bcac2f69d3679dcf8035c0e40a9fce6
Anis Mansour <anis@Aniss-MacBook-Pro.local>
2020-10-22 17:11:12 -0700

    added nav icon

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import './Footer.css';
+import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
+import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
+import SkipNextIcon from '@material-ui/icons/SkipNext';
+import ShuffleIcon from '@material-ui/icons/Shuffle';
+import ReplayIcon from '@material-ui/icons/Replay';
 
 function Footer() {
   return (
     <div className="footer">
-      <h1>footer</h1>
+      <div className="footer_left">
+        <p>Album and song details</p>
+      </div>
+      <div className="footer_center">
+        <ShuffleIcon className="option" />
+        <SkipPreviousIcon className="skip" />
+        <PlayCircleOutlineIcon fontSize="large" className="play" />
+        <SkipNextIcon className="skip" />
+        <ReplayIcon className="option" />
+      </div>
+      <div className="footer_right">
+        <p>Volume controls</p>
+      </div>
     </div>
   );
 }
